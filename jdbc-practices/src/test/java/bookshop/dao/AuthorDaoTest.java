681cc26104bfe38b19a5e5855c544d71f19c9a44
Konahyeon <koo@inha.edu>
2024-12-16 11:08:00 +0900

    bookshop

--- jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
+++ jdbc-practices/src/test/java/bookshop/dao/AuthorDaoTest.java
@@ -1,5 +1,27 @@
 package bookshop.dao;
 
-public class AuthorDaoTest {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import bookshop.vo.AuthorVo;
+
+public class AuthorDaoTest {
+	
+	private static AuthorDao dao = new AuthorDao();
+	private static AuthorVo vo = new AuthorVo();
+	
+	@Test
+	public void insertTest() {
+		vo.setName("칼세이건");
+		dao.insert(vo);
+		
+		assertNotNull(vo.getId());
+	}
+	
+	@AfterAll
+	public static void cleanup() {
+		 dao.deleteById(vo.getId());
+	}
 }
